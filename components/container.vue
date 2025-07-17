35734b51d44fad798a1f1b33132d933a983e94d9
yigitaydin <yigitaydin@trex.com.tr>
2025-07-17 15:05:01 +0300

    fix

--- components/container.vue
+++ components/container.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    home
+    asd
   </div>
 </template>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,10 @@
 // eslint.config.mjs
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu({
+  rules: {
+    'no-undef': 'off',
+  },
+  vue: true,
+  typescript: true,
+})
